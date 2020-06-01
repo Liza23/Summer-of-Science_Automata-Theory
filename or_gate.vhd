91fe8b62c372c06552d79fa2ea211afa86208859
Liza Dahiya <53112806+Liza23@users.noreply.github.com>
2020-06-02 00:05:00 +0530

    Create or_gate.vhd

---
+++ or_gate.vhd
@@ -0,0 +1,27 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity or_gate is
+port(	x in std_logic;
+			y in std_logic;
+			z out std_logic
+);
+end or_gate;
+
+architecture arch of or_gate is
+begin
+	process(x, y)
+	begin
+	if ((x='0') and (y='0')) then
+		z <= '0';
+	else 
+		z <= '1';
+	end if;
+	end process;
+
+end arch;
+
+architecture beh of or_gate is
+begin
+	z <='x' or 'y'
+end beh
