78228468ef0dd419715d681f30e45765be85eafb
Liza Dahiya <53112806+Liza23@users.noreply.github.com>
2020-06-02 11:48:47 +0530

    Update nor_gate.vhd

--- nor_gate.vhd
+++ nor_gate.vhd
@@ -1,1 +1,27 @@
+library ieee;
+use ieee.std_logic_1164.all;
 
+entity nor_gate is
+port(	x in std_logic;
+	y in std_logic;
+	z out std_logic
+);
+end nor_gate;
+
+architecture arch of nor_gate is
+begin
+	process(x, y)
+	begin
+	if ((x='0') and (y='0')) then
+		z <= '1';
+	else 
+		z <= '0';
+	end if;
+	end process;
+
+end arch;
+
+architecture beh of nor_gate is
+begin
+	z <='x' nor 'y'
+end beh
