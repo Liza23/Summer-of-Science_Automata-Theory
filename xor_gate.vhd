163ebb521f962d64836e8a31594a4d5816744c43
Liza Dahiya <53112806+Liza23@users.noreply.github.com>
2020-06-02 11:46:26 +0530

    Create xor_gate.vhd

---
+++ xor_gate.vhd
@@ -0,0 +1,27 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity xor_gate is
+port(	x in std_logic;
+	y in std_logic;
+	z out std_logic
+);
+end entity;
+
+architecture arch of xor_gate is 
+begin
+	process(x, y)
+	begin
+	if (x/=y) then 
+		z <='1';
+	else 
+		z <='0';
+	end if;
+	end process;
+	
+end arch;
+
+architecture beh of xor_gate is 
+begin 
+	z <= x xor y;
+end beh
