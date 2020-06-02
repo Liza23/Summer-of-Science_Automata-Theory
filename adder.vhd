2a483c6f22c7c43bec1c71fd4c825ff5a113eeb6
Liza Dahiya <53112806+Liza23@users.noreply.github.com>
2020-06-02 12:55:01 +0530

    Update adder.vhd

--- adder.vhd
+++ adder.vhd
@@ -5,6 +5,9 @@ use ieee.std_logic_unsigned.all;
 
 entity adder is 
 generic (n: natural :=2);
+--std_logic_vector gives an array of bits
+--downto gives the range of bits to be accesed
+--for eg. A(3 downto 0) will be A[3:0] 
 port( A:     in std_logic_vector(n-1 downto 0);
       B:     in std_logic_vector(n-1 downto 0);
       carry: out std_logic;
@@ -18,7 +21,9 @@ signal result: std_logic_vector(n downto 0);
 
 begin  
   result <= ('0' & A)+('0' & B);
+  --sum will be first n-1 bits of result
+  --carry will be the last or the most significant 
+  --bit of the result.
   sum <= result(n-1 downto 0);
   carry <= result(n);
-
 end beh;
